6610478a7cedfd48bf8bc9e7538d8fb35b5d157b
brianmituka <mitukab@outlook.com>
2017-12-11 17:59:03 +0300

    simplified the local storage code

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+11/12/2017
+refactored the whole code to manipulate DOM via strictly js
--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-document.getElementById("heading").innerHTML = localStorage['title'] || 'Heading goes here';
-document.getElementById('content').innerHTML = localStorage['text'] || 'Body of text editor';
-
-setInterval(function () {
-  localStorage['title'] = document.getElementById('heading').innerHTML;
-  localStorage['text'] = document.getElementById('content').innerHTML;
-
-}, 1000);
+var intervalSet;
 
 function addNote() {
   const heading = document.createElement('div');
+  heading.setAttribute('contenteditable', true)
+  heading.id = 'heading';
+
   const content = document.createElement('div');
-  heading.id = "heading";
-  content.id = "content";
+  content.setAttribute('contenteditable', true);
+  content.id = 'content';
+// setting the heading and content  to local storage default text object
+  heading.innerHtml = window.localStorage.getItem('title') || 'Heading goes here';
+  content.innerHtml = window.localStorage.getItem('text') || 'body of text editor';
+  // appending the divs to the DOM
+  document.getElementsByTagName("body")[0].append(heading);
+  document.getElementsByTagName("body")[0].append(content);
 
-  localStorage['title'] = document.getElementById('heading').innerHTML;
-  localStorage['text'] = document.getElementById('content').innerHTML;
+  // creating the interval function
+ setInterval(function () {
+    localStorage.setItem('title', document.getElementById('heading').innerHtml);
+    localStorage.setItem('text', document.getElementById('content').innerHtml);
 
+  }, 1000);
 }
